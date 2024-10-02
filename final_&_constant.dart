c3deeddb7700873d8aa4123b7c917d421c4c839e
Ibrahim Usman Kunji <ibrahim08062785621@gmail.com>
2024-10-02 14:43:45 +0100

    Update final_&_constant variable
    I add some content in  my  final_&_constant file

--- final_&_constant.dart
+++ final_&_constant.dart
@@ -1,1 +1,14 @@
+void main() {
+  // final keyword
+  final city = "Gombe";
+  final int num1 = 10; // writing the data type "int" is optional
 
+  //const keyword
+  const PI = 3.142;
+  const double gravity = 9.8; // writing the data type "double" is optional
+
+  print("I'm from  $city state ");
+  print("value of num1 is $num1");
+  print("The constant value of PI is $PI");
+  print("The constant value of acceleration due to gravity is $gravity");
+}
